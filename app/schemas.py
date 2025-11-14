1b9d417c3deecc6a41eb4959c6eee17db7e65714
Antoj2000 <147381287+Antoj2000@users.noreply.github.com>
2025-11-14 12:39:32 +0000

    first commit

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,47 @@
 from typing import Annotated, Optional
 from pydantic import BaseModel, EmailStr, Field, StringConstraints, ConfigDict
 
+from annotated_types import Ge, Le
+
+NameStr = Annotated[str, StringConstraints(min_length=1, max_length=100)]
+YearInt = Annotated[int, Ge(1900), Le(2100)]
+PageInt = Annotated[int, Ge(1), Le(10000)]
+TitleStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
+
+NameStr = Annotated[str, StringConstraints()]
+
+class AuthorRead(BaseModel):
+    id: int
+    name: NameStr
+    email: EmailStr
+    year_started: YearInt
+    model_config = ConfigDict(from_attributes=True)
+
+
+class AuthorCreate(BaseModel):
+    name: NameStr
+    email: EmailStr
+    year_started: YearInt
+
+class AuthorPatch(BaseModel):
+    name: Optional [NameStr] = None
+    email: Optional [EmailStr] = None
+    year_started: Optional [YearInt] = None
+
+
+class BookRead(BaseModel):
+    id: int
+    title: TitleStr
+    pages: PageInt
+    author_id: int
+    model_config = ConfigDict(from_attributes=True)
+
+class BookCreate(BaseModel):
+    title: TitleStr
+    pages: PageInt
+    author_id: int
+    
+class BookPatch(BaseModel):
+    title: Optional [TitleStr] = None
+    pages: Optional [PageInt] = None
+
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,2 +1,55 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from app.main import app, get_db
+from app.models import Base
+
+def author_payload(name="Anthony", email="anto@gmail.com", year_started= 1990):
+    return {
+        "name" : name,
+        "email" : email,
+        "year_started": year_started
+    }
+
+def book_payload(title="Something", pages= 600, author_id=1):
+    return {
+        "title" : title,
+        "pages" : pages,
+        "author_id": author_id
+    }
+
+def test_create_author(client):
+    r = client.post("/api/authors", json=author_payload())
+    assert r.status_code==201
+
+def test_author_dupe(client):
+    r = client.post("/api/authors", json=author_payload())
+    assert r.status_code==409
+    assert r.json()["detail"] == "User already exists"
+
+def  test_delete_then_404(client):
+    r = client.post("/api/authors", json = author_payload(email="delete@gmail.com"))
+    r = client.delete("/api/authors/2")
+    assert r.status_code==204
+    r = client.get("/api/users/2")
+    assert r.status_code==404
+
+def test_update_author(client):
+    r = client.post("/api/authors", json= author_payload(email="update@gmail.com"))
+    r = client.put("/api/authors/2", json= author_payload(name="Conor",email="update@gmail.com"))
+    assert r.status_code==200
+    data = r.json()
+    assert data["name"] == "Conor"
+
+def test_create_book(client):
+    r = client.post("/api/books", json=book_payload())
+    assert r.status_code==201
+
+
+
+
 
 
